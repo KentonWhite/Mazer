7b99f6e92485b7285bf015f7320817993ffcaf05
KentonWhite <jkentonwhite@gmail.com>
2010-05-23 20:15:40 -0400

    Initially scaffolding with nifty-layout and nifty-authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'd06fcd00b6211faa4a8a29fa4b63bef7f6a809144cb68c725440d9f7514566d25c81ea4593e0f901de3fac9e7c76fceb19a402a55cd9c2d8926224207fd9ffe7'
